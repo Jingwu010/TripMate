03e3e3cd359c6321da529bd113610acbaac8a7fa
Jingwu010 <GrEedWish_x@outlook.com>
2019-04-04 14:40:39 -0700

    changed DBQuery to static access, prevent multiple established connections to database

--- source/tripmate_cm/src/main/java/mysql/DBQuery.java
+++ source/tripmate_cm/src/main/java/mysql/DBQuery.java
@@ -2,6 +2,8 @@
  * Created by Jingwu Xu on 4/3/19.
  */
 
+package mysql;
+
 import java.sql.*;
 import java.util.*;
 
@@ -13,24 +15,26 @@ public class DBQuery {
     static String USERNAME = "tripmate";
     static String PASSWORD = "tripmate";
     static boolean DEBUG = false;
-    private Connection conn= null;
-    private Statement stmt = null;
+    private static Connection conn = null;
+    private static Statement stmt = connect_mysql();
 
-
-    public DBQuery() {
-        stmt = this.connect_mysql();
+    /**
+     * This method is only called for testing purpose
+     */
+    protected static void change_to_test_database() {
+        disconnect_mysql();
+        DATABASE = "tripmate_test";
+        stmt = connect_mysql();
     }
 
-    protected DBQuery(boolean test_flag) {
-        if (test_flag)
-            DATABASE = "tripmate_test";
-        stmt = this.connect_mysql();
-    }
-
-    private Statement connect_mysql() {
+    /**
+     * Establish a connection to mysql database and return a Statement interface
+     * @return A Statement interface
+     */
+    private static Statement connect_mysql() {
         String url = String.format(REGEX, IP, PORT, DATABASE, USERNAME, PASSWORD);
         System.out.println("[SQL CONNECTOR] -- url: " + url);
-        System.out.println("[SQL CONNECTOR] -- Connecting database...");
+        System.out.println("[SQL CONNECTOR] -- Connecting to database : " + DATABASE);
         try {
             conn = DriverManager.getConnection(url);
             System.out.println("[SQL CONNECTOR] -- Database connected!");
@@ -43,9 +47,11 @@ public class DBQuery {
         }
     }
 
-    private void disconnect_mysql() {
+    private static void disconnect_mysql() {
         try{
             conn.close();
+            stmt.close();
+            System.out.println("[SQL CONNECTOR] -- Disconnected from mysql database!");
         } catch (Exception e) {
             System.out.println("[SQL CONNECTOR] -- Error disconnecting from mysql database!");
             System.out.println(e);
@@ -53,42 +59,27 @@ public class DBQuery {
     }
 
     /**
-     * Cleans up connections once done
-     * NOT SURE HOW THIS WORKS
-     */
-    protected void finalize() throws Throwable {
-      // Invoke the finalizer of superclass
-      super.finalize();
-
-      // Close the Statement
-      stmt.close();
-
-      // Disconnect from MYSQL Database
-      this.disconnect_mysql();
-    }
-
-    /**
      * Query the 'state' table and retrieve full list of state names
      * @return A list of string indicates state name 
      */
-    protected List<String> get_all_states() {
-        return this.query_field_from_table("state_name", "state", "");
+    protected static List<String> get_all_states() {
+        return query_field_from_table("state_name", "state", "");
     }
 
     /**
      * Query the 'state' table and retrieve full list of state names
      * @return A list of string indicates city name 
      */
-    protected List<String> get_all_cities() {
-        return this.query_field_from_table("city_name", "city", "");
+    protected static List<String> get_all_cities() {
+        return query_field_from_table("city_name", "city", "");
     }
 
     /**
      * Query the 'poi' table and retrieve full list of poi names
      * @return A list of string indicates poi name      
      */
-    protected List<String> get_all_pois_names() {
-        return this.query_field_from_table("poi_name", "poi", "");
+    protected static List<String> get_all_pois_names() {
+        return query_field_from_table("poi_name", "poi", "");
     }
 
     /**
@@ -96,18 +87,18 @@ public class DBQuery {
      * @param  state_name A string that restricts the cities within a given state
      * @return            A list of string indicates city name 
      */
-    protected List<String> get_cities_by_state_name(String state_name) {
-        int state_id = this.get_state_id(state_name);
+    protected static List<String> get_cities_by_state_name(String state_name) {
+        int state_id = get_state_id(state_name);
         String constraint = String.format(" WHERE state_id_fk = %d", state_id);
-        return this.query_field_from_table("city_name", "city", constraint);
+        return query_field_from_table("city_name", "city", constraint);
     }
 
     /**
      * Query the 'poi' table and retrieve full list of poi details
      * @return A list of pois, each poi has multiple key-value pairs containing detailed information
      */
-    protected List<Map<String, String>> get_all_pois_details() {
-        return this.query_all_fields_from_table("poi", "");
+    protected static List<Map<String, String>> get_all_pois_details() {
+        return query_all_fields_from_table("poi", "");
     }
 
     /**
@@ -115,10 +106,10 @@ public class DBQuery {
      * @param  city_name A string that, if not empty, restricts the pois for a given city
      * @return           A list of pois, each poi has multiple key-value pairs containing detailed information
      */
-    protected List<Map<String, String>> get_pois_details_by_city_name(String city_name) {
-        int city_id = this.get_city_id(city_name);
+    protected static List<Map<String, String>> get_pois_details_by_city_name(String city_name) {
+        int city_id = get_city_id(city_name);
         String constraint = String.format(" WHERE city_id_fk = %d", city_id);
-        return this.query_all_fields_from_table("poi", constraint);
+        return query_all_fields_from_table("poi", constraint);
     }
 
     /**
@@ -130,7 +121,7 @@ public class DBQuery {
      * @param  extras     A string contains other specific restrictions on query
      * @return            A list of string that contains records in the field of that table with restrictions
      */
-    private List<String> query_field_from_table(String field_name, String table_name, String extras) {
+    private static List<String> query_field_from_table(String field_name, String table_name, String extras) {
         String query = String.format("SELECT %s FROM %s %s", field_name, table_name, extras);
         List<String> entries = new LinkedList<String>();
         try {
@@ -144,7 +135,15 @@ public class DBQuery {
         return entries;
     }
 
-    private List<Map<String, String>> query_all_fields_from_table(String table_name, String extras) {
+    /**
+     * Query the database, retrieve all data records for all field in a specific table, 
+     * other restrictions are allowed to append at the end
+     * The query follows the format "SELECT * FROM table_name extras"
+     * @param  table_name A string indicates the table that query operates on
+     * @param  extras     A string contains other specific restrictions on query
+     * @return            A list of string that contains records in the field of that table with restrictions
+     */
+    private static List<Map<String, String>> query_all_fields_from_table(String table_name, String extras) {
         String query = String.format("SELECT * FROM %s %s", table_name, extras);
         List<Map<String, String>> entries = new LinkedList<Map<String, String>>();
         try {
@@ -173,9 +172,9 @@ public class DBQuery {
      * @return            An integer that indicates the state_id
      * [TODO] handle illegal case
      */
-    private int get_state_id(String state_name) {
+    private static int get_state_id(String state_name) {
         String constraint = String.format(" WHERE state_name = '%s'", state_name);
-        return Integer.parseInt(this.query_field_from_table("state_id", "state", constraint).get(0));
+        return Integer.parseInt(query_field_from_table("state_id", "state", constraint).get(0));
     }
 
     /**
@@ -184,24 +183,23 @@ public class DBQuery {
      * @return           An integer that indicates the state_id
      * [TODO] handle illegal case
      */
-    private int get_city_id(String city_name) {
+    private static int get_city_id(String city_name) {
         String constraint = String.format(" WHERE city_name = '%s'", city_name);
-        return Integer.parseInt(this.query_field_from_table("city_id", "city", constraint).get(0));
+        return Integer.parseInt(query_field_from_table("city_id", "city", constraint).get(0));
     }
 
     public static void main(String args[]){
-        DBQuery con = new DBQuery();
-        List<String> states = con.get_all_states();
+        // DBQuery con = new DBQuery();
+        List<String> states = DBQuery.get_all_states();
         System.out.println(states.get(0));
-        System.out.println(con.get_state_id("Colorado"));
-        List<String> cities = con.get_all_cities();
+        System.out.println(DBQuery.get_state_id("Colorado"));
+        List<String> cities = DBQuery.get_all_cities();
         System.out.println(cities.get(10));
-        List<String> co_cities = con.get_cities_by_state_name("Colorado");
+        List<String> co_cities = DBQuery.get_cities_by_state_name("Colorado");
         System.out.println(co_cities.size());
         System.out.println(co_cities.get(3));
-        System.out.println(con.get_city_id("Vancouver"));
-
-        List<Map<String, String>> co_pois = con.get_all_pois_details();
+        System.out.println(DBQuery.get_city_id("Vancouver"));
+        List<Map<String, String>> co_pois = DBQuery.get_all_pois_details();
         System.out.println(co_pois.get(1));
     }
 }
